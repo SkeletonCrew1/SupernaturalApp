2b5fc3c7e9f227fa3f775cd3df093cbbc9a75304
Bohdan Holovchak <you@example.com>
2026-08-31 12:14:16 +0300

    fix:SKEL2-82 updated s3 name

--- terraform/s3-bucket-for-app/main.tf
+++ terraform/s3-bucket-for-app/main.tf
@@ -1,9 +1,9 @@
 resource "aws_s3_bucket" "app_bucket" {
-  bucket = "developer-bucket-images-704427427594-eu-north-1"
+  bucket = "developer-bucket-images-984546305184-eu-north-1"
 
   tags = {
     ManagedBy = "Terraform"
-    Name      = "developer-bucket-images-704427427594-eu-north-1"
+    Name      = "developer-bucket-images-984546305184-eu-north-1"
   }
 }
 
